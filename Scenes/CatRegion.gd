507baf7fde291245c5aebb9672fe4f2ec9029c86
koliao <francisco.roth8@gmail.com>
2024-05-13 14:41:17 -0300

    Rug and trashbin models

--- Scenes/CatRegion.gd
+++ Scenes/CatRegion.gd
@@ -1,10 +1,12 @@
 extends Node3D
 
 @export var opened = false
+@export var interaction_radius = 2.0
 
 func _process(delta):
 	$CatEyes/CatEyeLeft.opened = opened
 	$CatEyes/CatEyeRight.opened = opened
+	$Area3D/CollisionShape3D.shape.radius = interaction_radius * 1/scale.x
 
 func _on_area_3d_body_entered(body):
 	if(body.is_in_group("CanOpenCatRegion")):
